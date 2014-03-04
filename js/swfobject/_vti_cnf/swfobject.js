d0cdbcd3d309a86ad982bf215afcf2f5fbc1c2f8
Anthony Sinatra <asinatra@pertronix.com>
2014-03-04 11:23:26 -0800

    Initial Repository Upload for patriotpipes.com

---
+++ js/swfobject/_vti_cnf/swfobject.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|11 Jun 2009 08:44:00 -0000
+vti_extenderversion:SR|12.0.0.4518
+vti_backlinkinfo:VX|Business-Website/index.html clean_pro_html_template/index.html
+vti_author:SR|SCI\\sci00207
+vti_modifiedby:SR|SCI\\sci00207
+vti_timecreated:TR|11 Jun 2009 08:44:00 -0000
+vti_cacheddtm:TX|11 Jun 2009 08:44:00 -0000
+vti_filesize:IR|25560
